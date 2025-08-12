015c02981b15ce437fa42d280e4002829c4259a6
none <xiaomeili0117@gmail.com>
2025-08-12 17:52:31 +0800

    update runtime value

--- apps/api/api/index.ts
+++ apps/api/api/index.ts
@@ -1,9 +1,8 @@
 import serverless from 'serverless-http';
 import app from '../src/app';
 
-export const config = {
-  runtime: 'nodejs20.x'
-};
+// Use default Node.js runtime. Do not specify unsupported versions.
+export const config = { runtime: 'nodejs' };
 
 export default serverless(app);
 
